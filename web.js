8689220bb124b23b6b5879d32a128dc91e4db144
unknown <theaarishkhan18@yahoo.com>
2013-07-20 04:07:42 +0500

    first html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs=require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World! whsts up baby yeah enjoying it');
+
+fs.readFile('bitstarter.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+    response.send(data);
+});
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+    app.listen(port, function() {
   console.log("Listening on " + port);
 });
